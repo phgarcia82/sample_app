821a5a33bebd0462d41dae439579821374406538
ph <pierre.h.garcia@gmail.com>
2016-01-24 13:18:23 +0100

    Update pages_controller_spec.rb

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -44,6 +44,29 @@ RSpec.describe PagesController, type: :controller do
                                         "Simple App du Tutoriel Ruby on Rails | À Propos")
     end
   end
+  
+  describe "LayoutLinks" do
+
+  it "devrait trouver une page Accueil à '/'" do
+    get '/'
+    response.should have_selector('title', :content => "Accueil")
+  end
+
+  it "devrait trouver une page Contact at '/contact'" do
+    get '/contact'
+    response.should have_selector('title', :content => "Contact")
+  end
+
+  it "should have an À Propos page at '/about'" do
+    get '/about'
+    response.should have_selector('title', :content => "À Propos")
+  end
+
+  it "devrait trouver une page Iade à '/help'" do
+    get '/help'
+    response.should have_selector('title', :content => "Aide")
+  end
+end
 
 
 
