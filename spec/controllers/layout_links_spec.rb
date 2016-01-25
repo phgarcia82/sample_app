358d0679e05b17144c20b642529c5fa81593e64f
ph <pierre.h.garcia@gmail.com>
2016-01-25 11:35:02 +0100

    Delete layout_links_spec.rb

--- spec/controllers/layout_links_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe "LayoutLinks" do
-
-  it "devrait avoir une page d'inscription à '/signup'" do
-    get '/signup'
-    response.should have_selector('title', :content => "Inscription")
-  end
-end
